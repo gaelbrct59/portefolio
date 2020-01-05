9cfd32557f8424c2eebc466cdf6de02717b08372
Gael Bricout <gael.brct@gmail.com>
2020-01-05 11:51:40 +0100

    Update jeux.js

--- src/jeux.js
+++ src/jeux.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./components/css/containerWhite.css"
+import logoGB from './img/logoGB.png';
 
 export default () => {
   return(
     <div id="container">
       TODO
+      <img src={logoGB} />
     </div>
 
   )
