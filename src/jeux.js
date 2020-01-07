45065d3605f9c60ba67db633022e3570dc606c0c
Gael Bricout <gael.brct@gmail.com>
2020-01-07 23:27:53 +0100

    add projet nuit info 19

--- src/jeux.js
+++ src/jeux.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import "./components/css/containerWhite.css"
+import "./components/css/projets.css"
+
 import logoGB from './components/img/logoGB.png'
+import nuitinfo19 from './components/img/nuit2info19.png'
 export default () => {
   return(
     <div id="container">
-      TODO (aucun projet débuté pour le moment)
-      <img src={logoGB} width='150vw' height='150VH' style={{position:'absolute'}} />
+      <div id="liensVersProjets">
+        <a href = "http://51.83.255.96/" target = "_blank" > <img src={nuitinfo19} id="contenu" /> </a>
+        <img src={logoGB} width='150vw' height='150VH' style={{position:'absolute'}} id="contenu"/>
+      </div>
+      <div id="explicationsProjet">
+        <div id="explication_nuitInfo">
+          Je me suis occupé de la version Front (très rapidement) à l'aide de HTML/CSS(Bootstrap)
+          et de défis tels que le Gif (entièrement) et le <a href="http://51.83.255.96/ndinfo19/minigame.html" target="_blank">jeux 404</a> (une partie)
+        </div>
+      </div>
     </div>
 
   )
