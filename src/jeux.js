963bbcd191e0ee363a78b077c80fe353432f8586
gaelb <gael.brct@gmail.com>
2020-01-13 16:41:24 +0100

    fix ii2d pb

--- src/jeux.js
+++ src/jeux.js
@@ -1,11 +1,7 @@
 import React from 'react'
 import "./components/css/containerWhite.css"
 import "./components/css/projets.css"
-import "./ii2d/ii2d_primitive.js"
-import "./ii2d/ii2d_particle.js"
-import "./ii2d/ii2d_engine.js"
-import "./ii2d/ii2d_main.js"
-import "./ii2d/ii2d_obstacle.js"
+
 import logoGB from './components/img/logoGB.png'
 import nuitinfo19 from './components/img/nuit2info19.png'
 export default () => {
